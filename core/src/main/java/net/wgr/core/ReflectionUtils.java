71eb8371c53df00d25fd9ea9018f56bbcbfd3b29
Double-U <wannes321@gmail.com>
2012-01-23 23:30:15 +0100

    Improve ReflectionUtils - findClasses now works when .class directories are in a jar - Do not include transient fields in diff

--- core/src/main/java/net/wgr/core/ReflectionUtils.java
+++ core/src/main/java/net/wgr/core/ReflectionUtils.java
@@ -11,13 +11,16 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.security.CodeSource;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import org.apache.log4j.Logger;
 
 /**
@@ -53,35 +56,62 @@ public class ReflectionUtils {
     }
 
     /**
-     * Scans all classes accessible from the context class loader which belong to the given package and subpackages.
-     * Based on: http://snippets.dzone.com/posts/show/4831
+     * Scans all classes accessible from the context class loader which belong to the given package.
      *
      * @param packageName The base package
      * @return The classes
      * @throws ClassNotFoundException
      * @throws IOException
      */
-    public static List<Class> getClasses(String packageName)
-            throws ClassNotFoundException, IOException {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        assert classLoader != null;
-        String path = packageName.replace('.', '/');
-        Enumeration<URL> resources = classLoader.getResources(path);
-        List<File> dirs = new ArrayList<>();
-        while (resources.hasMoreElements()) {
-            URL resource = resources.nextElement();
-            dirs.add(new File(resource.getFile()));
+    public static List<Class> getClasses(String packageName, Class sourceContext) throws ClassNotFoundException, IOException {
+        CodeSource src = sourceContext.getProtectionDomain().getCodeSource();
+        List<Class> classes = new ArrayList<>();
+
+        if (src != null) {
+            URL jar = src.getLocation();
+            File f = null;
+
+            try {
+                f = new File(jar.toURI());
+            } catch (URISyntaxException ex) {
+                Logger.getLogger(ReflectionUtils.class).error("Illegal source path given by CodeSource", ex);
+                return classes;
+            }
+
+            if (f.isDirectory()) {
+                File target = new File(f, packageName.replace('.', '/'));
+                classes = findClasses(target, packageName);
+            } else {
+                ZipInputStream zip = new ZipInputStream(jar.openStream());
+                ZipEntry ze = null;
+                String path = packageName.replace('.', '/');
+
+                while ((ze = zip.getNextEntry()) != null) {
+                    if (ze.getName().startsWith(path) && ze.getName().endsWith(".class")) {
+                        // Remove leading slash
+                        int start = path.length() + 1;
+                        // Remove trailing .class extension
+                        int end = ze.getName().length() - 6;
+                        if (start > end) {
+                            continue;
+                        }
+
+                        String className = ze.getName().substring(start, end);
+                        if (!className.isEmpty() && !className.contains("$") && !className.contains("/")) {
+                            classes.add(Class.forName(packageName + '.' + className));
+                        }
+                    }
+                }
+            }
         }
-        ArrayList<Class> classes = new ArrayList<>();
-        for (File directory : dirs) {
-            classes.addAll(findClasses(directory, packageName));
-        }
+
         return classes;
     }
 
     /**
-     * Recursive method used to find all classes in a given directory and subdirs.
+     * Recursive method used to find all classes in a given directory.
      *
+     * @source http://snippets.dzone.com/posts/show/4831
      * @param directory   The base directory
      * @param packageName The package name for classes found inside the base directory
      * @return The classes
@@ -94,11 +124,23 @@ public class ReflectionUtils {
         }
         File[] files = directory.listFiles();
         for (File file : files) {
+            String fileName = file.getName();
             if (file.isDirectory()) {
-                assert !file.getName().contains(".");
-                classes.addAll(findClasses(file, packageName + "." + file.getName()));
-            } else if (file.getName().endsWith(".class")) {
-                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+                assert !fileName.contains(".");
+                // Do not recurse ATM
+                //classes.addAll(findClasses(file, packageName + "." + fileName));
+            } else if (fileName.endsWith(".class") && !fileName.contains("$")) {
+                Class clazz;
+                try {
+                    clazz = Class.forName(packageName + '.' + fileName.substring(0, fileName.length() - 6));
+                } catch (ExceptionInInitializerError e) {
+                    // happen, for example, in classes, which depend on 
+                    // Spring to inject some beans, and which fail, 
+                    // if dependency is not fulfilled
+                    clazz = Class.forName(packageName + '.' + fileName.substring(0, fileName.length() - 6),
+                            false, Thread.currentThread().getContextClassLoader());
+                }
+                classes.add(clazz);
             }
         }
         return classes;
@@ -121,7 +163,7 @@ public class ReflectionUtils {
             addAll = true;
             original = changed;
         }
-        
+
         if (changed == null) {
             addAll = true;
             changed = original;
@@ -129,9 +171,11 @@ public class ReflectionUtils {
 
         HashMap<String, Object> result = new HashMap<>();
         for (Field f : getAllFields(original.getClass())) {
-            
-            if (Modifier.isTransient(f.getModifiers())) continue;
-            
+
+            if (Modifier.isTransient(f.getModifiers())) {
+                continue;
+            }
+
             f.setAccessible(true);
             try {
                 Object origVal = f.get(original);
